acc40a5e8f0b30aa21e8b54ea52d150d024796b0
NicHahn <nici.hahn@web.de>
2020-07-14 15:31:04 +0200

    Replaced simple AI by MINIMAX-Algorithm

--- TicTacToe.py
+++ TicTacToe.py
@@ -51,24 +51,24 @@ def setStone(input, board, current_player):
     board[input] = current_player.symbol
 
 
-def checkForWin(board, player):
-    symbol = player.symbol
-    if board[1-1] == symbol and board[2-1] == symbol and board[3-1] == symbol:
-        return symbol
-    elif board[4-1] == symbol and board[5-1] == symbol and board[6-1] == symbol:
-        return symbol
-    elif board[7-1] == symbol and board[8-1] == symbol and board[9-1] == symbol:
-        return symbol
-    elif board[1-1] == symbol and board[4-1] == symbol and board[7-1] == symbol:
-        return symbol
-    elif board[2-1] == symbol and board[5-1] == symbol and board[8-1] == symbol:
-        return symbol
-    elif board[3-1] == symbol and board[6-1] == symbol and board[9-1] == symbol:
-        return symbol
-    elif board[1-1] == symbol and board[5-1] == symbol and board[9-1] == symbol:
-        return symbol
-    elif board[3-1] == symbol and board[5-1] == symbol and board[7-1] == symbol:
-        return symbol
+def checkForWin(board):
+    if board[1-1] == board[2-1] and board[2-1] == board[3-1] and board[1-1] != ' ':
+        return True, board[1-1]
+    elif board[4-1] == board[5-1] and board[5-1] == board[6-1] and board[4-1] != ' ':
+        return True, board[4-1]
+    elif board[7-1] == board[8-1] and board[8-1] == board[9-1] and board[7-1] != ' ':
+        return True, board[7-1]
+    elif board[1-1] == board[4-1] and board[4-1] == board[7-1] and board[1-1] != ' ':
+        return True, board[1-1]
+    elif board[2-1] == board[5-1] and board[5-1] == board[8-1] and board[2-1] != ' ':
+        return True, board[2-1]
+    elif board[3-1] == board[6-1] and board[6-1] == board[9-1] and board[3-1] != ' ':
+        return True, board[3-1]
+    elif board[1-1] == board[5-1] and board[5-1] == board[9-1] and board[1-1] != ' ':
+        return True, board[1-1]
+    elif board[3-1] == board[5-1] and board[5-1] == board[7-1] and board[3-1] != ' ':
+        return True, board[3-1]
+    return None, None
 
 
 def play_again():
@@ -77,10 +77,7 @@ def play_again():
 
 
 def copy_board(board):
-    copy = []
-    for i in board:
-        copy.append(i)
-    return copy
+    return baord.copy()
 
 
 def choose_random_move(board, list_moves):
@@ -92,7 +89,7 @@ def choose_random_move(board, list_moves):
     else:
         return None
 
-
+# simple AI to compute the move
 def get_ai_move(board, player, oponent):
     # 1. check for a winning move
     for i in range(0, 9):
@@ -108,7 +105,7 @@ def get_ai_move(board, player, oponent):
             setStone(i, copy, oponent)
             if checkForWin(copy, oponent):
                 return i
-  
+
     # 3. if possible place symbol in corner
     move_pos = choose_random_move(board, [0, 2, 6, 8])
     if move_pos != None:
@@ -123,9 +120,56 @@ def get_ai_move(board, player, oponent):
         return move_pos
 
 
+def best_move(board, ai):
+    # AI to make its turn
+    best_score = -1000
+    move = 0
+    for i in range(len(board)):
+        # is the spot available?
+        if board[i] == ' ':
+            board[i] = ai.symbol
+            score = minimax(board, 2, False)
+            board[i] = ' '
+            if score > best_score:
+                best_score = score
+                move = i
+    return move
+
+
+def minimax(board, depth, is_maximizer):
+    scores = [0, 1, -1]
+    win, player = checkForWin(board)
+    if win:
+        return scores[1] if player == 'O' else scores[2]
+    if gameEnd(board):
+        return scores[0]
+        # if it's not a final state
+    if is_maximizer:
+        best_value = -1000
+        for i in range(len(board)):
+            # is the spot available?
+            if board[i] == ' ':
+                board[i] = 'O'
+                value = minimax(board, depth + 1, False)
+                # Delete tested spot ... you could also do a copy of the board
+                board[i] = ' '
+                best_value = max(value, best_value)
+        return best_value
+    else:
+        best_value = 1000
+        for i in range(len(board)):
+            # is the spot available?
+            if board[i] == ' ':
+                board[i] = 'X'
+                value = minimax(board, depth + 1, True)
+                board[i] = ' '
+                best_value = min(value, best_value)
+        return best_value
+
+
 def gameLoop():
-    # Reset the board
     while True:
+        # Reset the board
         board = [' '] * 9
         player_1 = Player('X')
         player_2 = Player('O')
@@ -156,21 +200,22 @@ def gameLoop():
         while not gameEnd(board):
             printPlayground(board)
             print()
+            #Check if AI is selected
             if current_player == player_2 and modus == 'c':
                 print("Computer is on move")
-                setStone(get_ai_move(board, player_2, player_1),
+                setStone(best_move(board, current_player),
                          board, current_player)
             else:
-                inputPlayer = playerInput(board, current_player)
+                inputPlayer = playerInput(board, player_1)
                 if inputPlayer == 'q':
                     break
                 else:
                     setStone(inputPlayer, board, current_player)
 
-            win = checkForWin(board, current_player)
+            win, player = checkForWin(board)
             if win:
                 printPlayground(board)
-                print("{} won the game.".format(win))
+                print("{} won the game.".format(player))
                 break
 
             current_player = changePlayer(current_player, player_1, player_2)
